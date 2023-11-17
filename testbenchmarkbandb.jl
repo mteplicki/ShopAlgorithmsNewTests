c54b4ac86ed3c8e954405172d70e98774160407d
mteplicki <261726@student.pwr.edu.pl>
2023-11-17 10:17:25 +0100

    ok

--- testbenchmarkbandb.jl
+++ testbenchmarkbandb.jl
@@ -4,15 +4,15 @@ using Distributed
 
 const timeout::Int = 4000
 
-# instances = load_instances("testsFromBenchmarks")
+instances = load_instances("testsFromBenchmarks")
 
-# filter!(x->(x.n<=15 && x.m <= 10), instances)
+filter!(x->(x.n<=15 && x.m <= 10), instances)
 
-# my_functions = get_functions("Branch and Bound - 1|R_j|Lmax", "Branch and Bound - Carlier")
+my_functions = get_functions("Branch and Bound - 1|R_j|Lmax", "Branch and Bound - Carlier")
 
-# instances_with_functions = mix_instances_with_functions(instances, my_functions)
+instances_with_functions = mix_instances_with_functions(instances, my_functions)
 
-# make_tests(instances_with_functions, timeout, "results/resultsFromBenchmarks/resultbandb.csv"; garbage_collect=true)
+make_tests(instances_with_functions, timeout, "results/resultsFromBenchmarks/resultbandb.csv"; garbage_collect=true)
 
 instances = load_instances("testsFromBenchmarks")
 
--- testbenchmarkheuristic.jl
+++ testbenchmarkheuristic.jl
@@ -3,12 +3,12 @@ using Distributed
 
 @everywhere include("test_maker.jl")
 
-timeout = 240
+timeout = 480
 
 instances = load_instances("testsFromBenchmarks")
 
-functions = get_functions("Shifting Bottleneck", "Shifting Bottleneck - DPC", "Shifting Bottleneck - DPC with stack", "Shifting Bottleneck - DPC with timeout 0.5 with depth 0", "Shifting Bottleneck - DPC with timeout 0.5 with depth 1", "Shifting Bottleneck - DPC with timeout 10.0 with depth 0", "Shifting Bottleneck - DPC with timeout 10.0 with depth 1")
+functions = get_functions("Shifting Bottleneck", "Shifting Bottleneck - DPC", "Shifting Bottleneck - Carlier", "Shifting Bottleneck - DPC with timeout 0.5 with depth 0", "Shifting Bottleneck - DPC with timeout 0.5 with depth 1", "Shifting Bottleneck - DPC with timeout 10.0 with depth 0", "Shifting Bottleneck - DPC with timeout 10.0 with depth 1")
 
 instances_with_functions = mix_instances_with_functions(instances, functions)
 
-make_tests(instances_with_functions, timeout, "results/resultsFromBenchmarks/result_sb.csv")
+make_tests(instances_with_functions, timeout, "results/resultsFromBenchmarks/resultheuristics.csv")
